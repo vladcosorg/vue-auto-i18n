f16fed4bd855e79c5db939ad19aa873d4cdadc44
chetzof <vcosvic@gmail.com>
2020-12-11 14:56:15 +0200

    build: tuned tsconfig.json to improve type-checkinmg

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   settings: {
     'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
+      '@typescript-eslint/parser': ['.ts'],
     },
     'import/resolver': {
       typescript: {}, // this loads <rootdir>/tsconfig.json to eslint
