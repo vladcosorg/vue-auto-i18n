77f555c1a9985532948107db705fc10228ecc048
chetzof <vcosvic@gmail.com>
2020-12-12 17:54:25 +0200

    feat: added better exceptions with meaninful errors

---
+++ test/unit/error.test.ts
@@ -0,0 +1,28 @@
+import { InformativeError } from '@/error'
+// eslint-disable-next-line @typescript-eslint/no-empty-interface,import/no-unused-modules
+interface CircularObject extends Record<string, string | CircularObject> {}
+
+test('Contextual circular object is properly formatted', () => {
+  const testObject: CircularObject = { plain: 'value' }
+  testObject['circular'] = testObject
+
+  const error = new InformativeError('Test message', { testObject })
+  expect(error.toString()).toEqual(`Error: Test message with context
+ {
+  "testObject": {
+    "plain": "value",
+    "circular": "#CIRCULAR#"
+  }
+}`)
+})
+
+test('Deep object is truncated', () => {
+  const testObject: CircularObject = { deep: { object: { is: 'truncated' } } }
+  const error = new InformativeError('Test message', { testObject })
+  expect(error.toString()).toEqual(`Error: Test message with context
+ {
+  "testObject": {
+    "deep": "#TRUNCATED#"
+  }
+}`)
+})
