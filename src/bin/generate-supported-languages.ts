4fccb820e7a00a6ae68d432e5f32551e7b74c6bc
chetzof <vcosvic@gmail.com>
2020-12-21 11:43:52 +0200

    Migrated to node-fetch

--- src/bin/generate-supported-languages.ts
+++ src/bin/generate-supported-languages.ts
@@ -1,21 +1,23 @@
-import { join } from 'path'
-
-import axios from 'axios'
+// eslint-disable-next-line import/no-unused-modules
 import { config } from 'dotenv'
 import { outputFile } from 'fs-extra'
 import { format } from 'prettier'
+import fetch from 'node-fetch'
 
 config()
+
 async function run(): Promise<void> {
-  const response = await axios.get<{
-    data: { languages: { language: string }[] }
-  }>('https://translation.googleapis.com/language/translate/v2/languages', {
-    params: new URLSearchParams({
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      key: process.env.GOOGLE_API_KEY!,
-    }),
-  })
-  const languages = response.data.data.languages.map((entry) => entry.language)
+  const request = await fetch(
+    'https://translation.googleapis.com/language/translate/v2/languages?' +
+      new URLSearchParams({
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        key: process.env.GOOGLE_API_KEY!,
+      }),
+  )
+  const response = await request.json()
+  const languages = response.data.languages.map(
+    (entry: { language: string }) => entry.language,
+  )
   await outputFile(
     './dist/supported-languages/google.json',
     format(JSON.stringify(languages), {
