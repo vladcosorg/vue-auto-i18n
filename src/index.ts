4db0aa624a5da05ed91e42e3cdeb01fbad39f593
chetzof <vcosvic@gmail.com>
2021-01-10 14:13:27 +0200

    fix:  migrated to tsdx

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { integrateWithVueI18n, ManualTranslator } from './integration/vue-i18n'
 import { translateMessageObject } from './translator'
 import { TranslationService } from './translation-service/translation-service'
 import { CacheType } from './cache/cache-type'
+import { InMemoryCache } from './cache/type/in-memory-cache'
 
 export {
   integrateWithVueI18n,
@@ -9,4 +10,5 @@ export {
   TranslationService,
   CacheType,
   ManualTranslator,
+  InMemoryCache,
 }
