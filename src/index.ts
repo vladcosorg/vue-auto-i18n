d223a5656cb18f709483ec7e66ab01f7de017b7e
chetzof <vcosvic@gmail.com>
2021-01-10 13:54:10 +0200

    fix:  migrated to tsdx

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
 import { integrateWithVueI18n } from './integration/vue-i18n'
 import { translateMessageObject } from './translator'
+import { TranslationService } from './translation-service/translation-service'
+import { CacheType } from './cache/cache-type'
 
-export { integrateWithVueI18n, translateMessageObject }
+export {
+  integrateWithVueI18n,
+  translateMessageObject,
+  TranslationService,
+  CacheType,
+}
