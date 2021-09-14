96b99ae33c56f2dfea2dfffa51348fe65ea9ddc0
chetzof <vcosvic@gmail.com>
2021-09-14 19:41:34 +0300

    fix: Added support for partial translation in order to save on API quota's

--- test/unit/service/google-free-translation.test.ts
+++ test/unit/service/google-free-translation.test.ts
@@ -2,7 +2,7 @@ import { GoogleFree } from '../../../src/translation-service/google-free'
 
 const api = new GoogleFree()
 
-describe('Live API (may be unstable)', () => {
+describe.skip('Live API (may be unstable)', () => {
   test('Simple string returns translation', async () => {
     await expect(api.translate('ru', { foo: 'hi' })).resolves.toEqual({
       foo: 'Привет',
